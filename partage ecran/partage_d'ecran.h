ed16cadd14c61250df69fc859eaf2c93f1c54e7f
achrafbouslama123 <achraf.bouslama@esprit.tn>
2020-06-16 12:28:24 +0100

    ss

---
+++ partage ecran/partage_d'ecran.h
@@ -0,0 +1,32 @@
+/**
+* @file main.c
+* @autor hassen mabrouk
+* @date  Mai ,2020
+*
+*testing program for game share screen 
+*
+*/
+#include <SDL/SDL.h>
+#define largeur 1200
+#define hauteur 600
+typedef struct joueur joueur;
+
+struct joueur{
+SDL_Surface *p[6];
+SDL_Rect pp;
+};
+
+typedef struct background background;
+
+struct background{
+SDL_Surface *background;
+SDL_Rect pb;
+};
+
+void initialiser_joueur1(joueur *j);
+void initialiser_joueur2(joueur *j);
+
+void initialiser_background1(background *b);
+void initialiser_background2(background *b);
+
+
